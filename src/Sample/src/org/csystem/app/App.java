8d9f3aa42c6a39ccb41857ac1f8b180ea2591b0e
Oğuz Karan <oguzkaran@csystem.org>
2022-08-06 12:22:50 +0300

    Update

--- src/Sample/src/org/csystem/app/App.java
+++ src/Sample/src/org/csystem/app/App.java
@@ -1,35 +1,38 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Sınıflar arası ilişkiler: Sınıflar arası ilişkiler aslında nesneler arasındaki ilişkiler olarak düşünülmeldir. Örneğin,
-	uçak ile pilotları arasında bir ilişki vardır. Ya da araba ile motoru arasında da bir ilişki vardır. Burada uçak
-	nesnesinin pilot nesneleri arasındaki ilişkiden bahsedilir. Benzer şekilde araba nesnesinin motor nesnesi olan
-	ilişkiden bahsedilmiş olur. Ancak şüphesiz bu ilişkilere uygun olarak sınıflar yazılmalıdır.
-
-	Nesne yönelimli programlama tekniği kullanılarak geliştirilecek bir projenin kodlama aşamasına gelindiğinde önce sınıflar
-	ve aralarındaki ilişkiler belirlenir. Sonra kodlamaya geçilir. Sınıflar ve aralarındaki ilişkiler belirlenirken, sınıfların
-	ve nesnelerin konuya (domain) ilişkin durumları düşünülür.
-
-	Anahtar Notlar: Bir projenin müşteri ile görüşülmesinden (ihtiyaçların belirlenmesi için) teslime (deployment) kadar
-	geçen sürecin çeşitli şemalar ile anlatılmasını sağlayan UML (Unified Modeling Language) denilen bir araç bazı
-	durumlarda kullanılabilmektedir. Bu aracın önemli ve geliştiriciler ilgilendiren şemalarından birisi
-	"sınıf şeması (class diagram)"'dır. Bu şema, kodlamaya yönelik ve kodlamaya başlamadan önce yapılır. Bu şemada sınıfların
-	detayları ve aralarındaki ilişkiler çizilir.
-
-	Anahtar Notlar: UML her zaman ve her detayı ile kullanılmalı mıdır? Bunun için verilecek cevap nettir: Hayır.
-	Gerektiğinde ve gerektiği kadar kullanılmalıdır.
-
-
+	A ile B arasındaki "association" ilişkisi
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
 class App {
 	public static void main(String [] args)
 	{
+		B b1 = new B(/*...*/);
+		B b2 = new B(/*...*/);
+		A a = new A();
 
+		a.doWork(b1);
+
+		//...
+
+		a.doWork(b2);
 	}
 }
 
 
+class A {
+	//...
 
+	public void doWork(B b)
+	{
+		b.doSomething();
+		//...
+	}
+}
 
-
-
+class B {
+	//...
+	public void doSomething()
+	{
+		//...
+	}
+}
