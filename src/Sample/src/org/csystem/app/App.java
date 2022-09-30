1b62e8cc9a3fa52eb868fa868686f955c5190698
Oğuz Karan <oguzkaran@csystem.org>
2022-09-30 23:41:05 +0300

    Update

--- src/Sample/src/org/csystem/app/App.java
+++ src/Sample/src/org/csystem/app/App.java
@@ -1,6 +1,7 @@
 /*----------------------------------------------------------------------------------------------------------------------
-	Throwable parametreli catch bloğu ile tüm fırlatılan exception nesneleri yakalanabilir. Şüphesiz Throwable parametreli
-	catch bloğunun tüm catch bloklarından sonra yazılması gerekir
+	try-finally yani catch bloksuz try deyimi ile exception nesnesi yakalanmadan ve aynı zamanda exception fırlatılsa da
+	fırlatılmasa da yapılacak bir işleme yönelik kod yazılabilir. Aşağıdaki örneği çeşitli değerler ile çalıştırarak
+	sonuçları gözlemleyiniz
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
 
@@ -10,12 +11,7 @@ class App {
 	public static void main(String [] args)
 	{
 		try {
-			Scanner kb = new Scanner(System.in);
-			System.out.print("Bir sayı giriniz:");
-			double val = Double.parseDouble(kb.nextLine());
-			double result = MathUtil.log10(val);
-
-			System.out.printf("log10(%f) = %f%n", val, result);
+			Util.doWork();
 		}
 		catch (IndeterminateException ex) {
 			System.out.println("Belirsiz");
@@ -23,11 +19,30 @@ class App {
 		catch (UndefinedException ex) {
 			System.out.println("Tanımsız");
 		}
-		catch (Throwable ex) {
-			System.out.println("Exception oluştu");
+		finally {
+			System.out.println("main:finally");
 		}
 
 		System.out.println("Tekrar yapıyor musunuz?");
+	}
+}
+
+class Util {
+	public static void doWork()
+	{
+		try {
+			Scanner kb = new Scanner(System.in);
+			System.out.print("Bir sayı giriniz:");
+			double val = Double.parseDouble(kb.nextLine());
+			double result = MathUtil.log10(val);
+
+			System.out.printf("log10(%f) = %f%n", val, result);
+		}
+		finally {
+			System.out.println("doWork:finally");
+		}
+
+		System.out.println("doWork sonu");
 	}
 }
 
