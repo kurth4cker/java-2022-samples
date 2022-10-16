74609961132255fec1d2539fea9aaffd5dc9c3f1
Oğuz Karan <oguzkaran@csystem.org>
2022-10-16 22:11:03 +0300

    Update

--- src/Sample/src/org/csystem/app/App.java
+++ src/Sample/src/org/csystem/app/App.java
@@ -1,11 +1,19 @@
 /*----------------------------------------------------------------------------------------------------------------------
-
+	twr bloğu tek başına olabilir. Scanner sınıfı Closeable arayüzünü destekler
 ----------------------------------------------------------------------------------------------------------------------*/
 package org.csystem.app;
+
+import java.util.Scanner;
 
 class App {
 	public static void main(String [] args)
 	{
+		try (Scanner kb = new Scanner(System.in)) {
+			System.out.print("Bir sayı giriniz:");
+			int val = Integer.parseInt(kb.nextLine());
 
+			System.out.println(val * val);
+		}
 	}
 }
+
